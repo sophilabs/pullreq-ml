f22546f7be46661624de7b3e13a222f340fa8b89
shoval-lev-hippo <slev@hippo.com>
2021-12-23 10:35:30 +0200

    frontend

---
+++ frontend/src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	import HeaderBar from './components/HeaderBar.svelte';
+	import Header from './components/header/Header.svelte';
+	import SubmitButton from './components/submitButton/SubmitButton.svelte';
+	export let header;
+</script>
+
+<main>
+	<HeaderBar />
+	<Header header={header}/>
+	<input placeholder="Github link">
+	<SubmitButton/>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ frontend/src/components/header/Header.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let header;
+</script>
+
+
+<h1>{header}!</h1>
+
+<style>
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
